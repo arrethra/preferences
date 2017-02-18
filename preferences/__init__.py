f5d6298d4188bc17d41f21a4bdb2028fbffb1146
Arrethra <arrethra@gmail.com>
2017-02-18 17:29:02 +0100

    previous merge failed. Does it now succeed?

--- preferences/__init__.py
+++ preferences/__init__.py
@@ -83,8 +83,8 @@ class Preferences():
             path = os.path.abspath(path) # in case input was not formatted correctly
             path_to_filename = os.path.split(path)[0]
             if not os.path.isabs(path_to_filename):
-                error_message = "No such directory: %s."%filename
-                raise FileNotFoundError(error_message) # Is this the correct version of error to be raised?
+                error_message = "Entered filename '%s' does not lead to exisiting directory."%filename
+                raise OSError(error_message) # Is this the correct version of error to be raised?
             else:
                 self._filename_to_store_the_preferences = path
         else:
